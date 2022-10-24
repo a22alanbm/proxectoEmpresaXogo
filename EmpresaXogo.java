8a57dde572b7cd6bf696d98c1507c54de74d44aa
dam1 - Barreiro Martínez <A22AlanBM@sanclemente.local>
2022-10-24 17:36:27 +0200

    Engadindo gitignore

---
+++ EmpresaXogo.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.Math;
+
+public class EmpresaXogo {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calcularEntrada();
+    } 
+    public static void calcularEntrada() {
+        int prezo;
+        int idade;
+        
+        System.out.println("Indica a idade do cliente.");
+        idade = input.nextInt();
+        if (idade < 0) {
+            System.out.println("A idade non é valida.");
+        } else {
+            if (idade < 4 && idade >= 0) {
+                prezo = 0;
+            } else {
+                if (idade < 18) {
+                    prezo = 5;
+                } else {
+                    prezo = 10;
+                }
+            }
+            System.out.println("O prezo da entrada é:" + prezo + "euros.");
+        }
+    }
+}
